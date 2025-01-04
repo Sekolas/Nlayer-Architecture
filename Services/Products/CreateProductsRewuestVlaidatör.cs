7e8c2296fa2afe68c691c6e6db5273fa3ed736fb
Serkan Kemer <115104251+Sekolas@users.noreply.github.com>
2025-01-05 00:40:56 +0300

    validations

--- Services/Products/CreateProductsRewuestVlaidatör.cs
+++ Services/Products/CreateProductsRewuestVlaidatör.cs
@@ -15,7 +15,7 @@ namespace Services.Products
             RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required")
                 .Length(3, 10).WithMessage("Name must be between 3-10 characters");
             RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");       
-            RuleFor(x => x.stock).GreaterThan(0).WithMessage("Stock must be greater than 0");
+            RuleFor(x => x.stock).InclusiveBetween(1, 100).WithMessage("Stock must be between 1-100");
         }
     }
 }
